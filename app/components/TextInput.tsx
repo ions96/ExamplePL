898d220dc62ce6b42b214d31dd4198684ac612d8
IonSochirca <58588897+ions96@users.noreply.github.com>
2023-12-13 19:26:56 +0200

    add input and button component

---
+++ app/components/TextInput.tsx
@@ -0,0 +1,67 @@
+import React, {forwardRef, useState} from 'react';
+import {
+  StyleSheet,
+  TextInput as RNTextInput,
+  TextInputProps as RNTextInputProps,
+} from 'react-native';
+import Text from './Text';
+
+export interface TextInputProps extends RNTextInputProps {
+  title?: string;
+  error?: string;
+}
+
+export const TextInput = forwardRef<RNTextInput, TextInputProps>(
+  ({error, title, style, ...props}: TextInputProps, ref) => {
+    const [borderColor, setBorderColor] = useState('#C9CDD1');
+    return (
+      <>
+        {title ? (
+          <Text variant="medium14" style={[style, styles.title]}>
+            {title}
+          </Text>
+        ) : null}
+        <RNTextInput
+          ref={ref}
+          underlineColorAndroid={'rgba(0,0,0,0)'}
+          multiline={false}
+          onFocus={() => {
+            setBorderColor('#1677FF');
+          }}
+          onBlur={() => {
+            setBorderColor('#C9CDD1');
+          }}
+          {...props}
+          style={[styles.input, {borderColor: borderColor}]}
+        />
+
+        {error ? (
+          <Text variant="medium14" style={styles.error}>
+            {error}
+          </Text>
+        ) : null}
+      </>
+    );
+  },
+);
+export default React.memo(TextInput);
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 0.7,
+    width: '100%',
+    padding: 11,
+    borderRadius: 8,
+    color: '#5E6166',
+    fontSize: 17,
+    fontWeight: '400',
+  },
+  error: {
+    color: '#FF4D4F',
+    paddingTop: 6,
+  },
+  title: {
+    color: '#878C91',
+    paddingBottom: 6,
+  },
+});
